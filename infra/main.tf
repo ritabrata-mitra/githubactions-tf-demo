aad90b1aef34a8bdb4f406c9553be798e058903e
ritabrata-mitra <105199843+ritabrata-mitra@users.noreply.github.com>
2026-02-25 15:33:42 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 }
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0bb661687d1decf72"
+  ami           = "ami-123"
   instance_type = "t3.micro"
   tags = {
     Name = "my-ec2-instance"
