4d1316796ee1413377d01d5c522e77653529eff0
ritabrata-mitra <105199843+ritabrata-mitra@users.noreply.github.com>
2026-03-04 15:38:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
     key            = "network/vpc/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    
   }
 }
 
